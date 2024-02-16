96b808ea4923d4ec906c3680217d45e28f176c08
tebr1z <tabrizyh@code.edu.az>
2024-02-16 14:18:33 +0400

    Feat:Task1,Task2

---
+++ #hometask_Array/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+#region task1
+//int[] numbers = { 34,55 };
+
+//int sum = 0;    
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+//}
+// Console.WriteLine(sum);
+#endregion
+
+#region task2
+
+int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+int cem = numbers[0] + numbers[numbers.Length-1];
+Console.WriteLine(cem);
+
+
+#endregion
